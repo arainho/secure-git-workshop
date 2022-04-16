8cffb9c35e23a1c16dd301a2c4b1dc5a5fd16835
André Rainho <arainho.it@gmail.com>
2022-04-16 23:03:31 +0100

    Update sonar-scanner.sh

--- samples/sonar-scanner.sh
+++ samples/sonar-scanner.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export SONAR_SCANNER_VERSION=4.7.0.2747
+export SONAR_SCANNER_HOME=$PWD/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+curl --create-dirs -sSLo $PWD/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+unzip -o $PWD/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+export SONAR_SCANNER_OPTS="-server"
+
 sonar-scanner \
   -Dsonar.organization=xyz-workshops \
   -Dsonar.projectKey=git-secure-workshop \
