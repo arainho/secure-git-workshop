f13879c849be1af56f6d8d5322e0c0c841524609
André Rainho <arainho.it@gamil.com>
2022-04-21 20:10:20 +0100

    sonar scanner script sample

--- samples/sonar-scanner.sh
+++ samples/sonar-scanner.sh
@@ -3,9 +3,11 @@
 export SONAR_SCANNER_VERSION=4.7.0.2747
 export SONAR_SCANNER_HOME=$PWD/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
 curl --create-dirs -sSLo $PWD/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
-unzip -o $PWD/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+unzip -o $PWD/.sonar/sonar-scanner.zip -d $PWD/.sonar/
 export PATH=$SONAR_SCANNER_HOME/bin:$PATH
 export SONAR_SCANNER_OPTS="-server"
+
+if [ ! -z $SONAR_TOKEN]; then exit 1; fi
 
 sonar-scanner \
   -Dsonar.organization=xyz-workshops \
