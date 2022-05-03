e8a6b65f87f24b5e096d7c3b190e4cb20f7fcbff
André Rainho <arainho.it@gmail.com>
2022-05-03 09:30:48 +0100

    changes on py-web code, fix port and add healthcheck

--- rest-api-goat/py-web/simple_server.py
+++ rest-api-goat/py-web/simple_server.py
@@ -1,11 +1,11 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import http.server
+import socketserver
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
-    """Entrypoint for python server"""
-    server_address = ("0.0.0.0", 8000)
-    httpd = server_class(server_address, handler_class)
-    print("launching server...")
+HOST, PORT = "0.0.0.0", 9999
+address = (HOST, PORT)
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(address, Handler) as httpd:
+    print (f"Serving at {HOST} on port {PORT}.")
     httpd.serve_forever()
-    
-if __name__ == "__main__":
-    run()
